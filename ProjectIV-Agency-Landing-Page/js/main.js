ced28557f302c1f739c5f3c3ff346fff8aad73ab
Yuting Chou <ytingchou@gmail.com>
2016-09-21 21:38:31 +0800

    Added scrolling navbar effect.

---
+++ ProjectIV-Agency-Landing-Page/js/main.js
@@ -0,0 +1,17 @@
+$(window).scroll(function() {
+  if($('.navbar').offset().top > 50) {
+    $('.navbar-fixed-top').addClass('top-nav-collapse');
+  } else {
+    $('.navbar-fixed-top').removeClass('top-nav-collapse');
+  }
+});
+
+$(function() {
+  $('.page-scroll a').bind('click', function() {
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 1500);
+    event.preventDefault();
+  })
+})
